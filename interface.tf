bd7a2d165cccccfdc3e12c7718be798d4e900cb4
rintonurtantho <rinto.nurtantho@hotmail>
2026-02-12 17:22:09 +0700

    Initial Azure VM destroy using Terraform

--- interface.tf
+++ interface.tf
@@ -3,7 +3,25 @@ resource "azurerm_public_ip" "pip" {
   name                = "pip-terraform-basic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
+# Public IP untuk dockertest1
+resource "azurerm_public_ip" "pip1" {
+  name                = "pip-dockertest1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
+# Public IP untuk dockertest2
+resource "azurerm_public_ip" "pip2" {
+  name                = "pip-dockertest2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
   sku                 = "Basic"
 }
 
@@ -20,3 +38,32 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
+
+# NIC untuk dockertest1
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic-dockertest1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip1.id
+  }
+}
+
+# NIC untuk dockertest2
+resource "azurerm_network_interface" "nic2" {
+  name                = "nic-dockertest2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip2.id
+  }
+}
+
--- vm.tf
+++ vm.tf
@@ -1,20 +1,20 @@
-# Virtual Machine Ubuntu
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-ubuntu-basic"
+# VM Linux dockertest1
+resource "azurerm_linux_virtual_machine" "dockertest1" {
+  name                = "dockertest1"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B2als_v2"
-  admin_username      = "rinto"
   network_interface_ids = [
-    azurerm_network_interface.nic.id,
+    azurerm_network_interface.nic1.id,
   ]
-
+  
 # Gunakan password authentication
   disable_password_authentication = false
-  admin_password                  = var.admin_password
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
 
   os_disk {
-    name                 = "osdisk-ubuntu-basic"
+    name                 = "osdisk-dockertest1"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -26,3 +26,33 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
+# VM Linux dockertest2
+resource "azurerm_linux_virtual_machine" "dockertest2" {
+  name                = "dockertest2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2als_v2"
+  network_interface_ids = [
+    azurerm_network_interface.nic2.id,
+  ]
+  
+# Gunakan password authentication
+  disable_password_authentication = false
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  os_disk {
+    name                 = "osdisk-dockertest2"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
+
